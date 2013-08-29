dd78ae9afd91d7251366a726db0110a7fc0d2358
Matthew Wedgwood <mwedgwood@gmail.com>
2013-08-29 13:24:18 -0500

    More control over output/verbosity

--- codetree/cli.py
+++ codetree/cli.py
@@ -4,12 +4,22 @@ from .config import Config
 
 
 def main():
-    logfmt = "%(msg)s"
-    logging.basicConfig(level=logging.INFO, format=logfmt)
-
     ap = ArgumentParser()
     ap.add_argument("cfgfile", nargs="+", help="Codetree configuration file")
+    verbosity = ap.add_mutually_exclusive_group(required=False)
+    verbosity.add_argument("-v", "--verbose", action="store_true", default=False)
+    verbosity.add_argument("-q", "--quiet", action="store_true", default=False)
 
     args = ap.parse_args()
+
+    logfmt = "%(message)s"
+    loglevel = logging.INFO
+    if args.verbose:
+        logfmt = "%(levelname)s: %(message)s"
+        loglevel = logging.DEBUG
+    if args.quiet:
+        loglevel = logging.CRITICAL
+    logging.basicConfig(format=logfmt, level=loglevel)
+
     config = Config(args.cfgfile)
     config.build()
